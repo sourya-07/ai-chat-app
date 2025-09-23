9cae451058fae1b8a3e756b7c3421c3bac2bc091
Sourya <souryagupta2005@gmail.com>
2025-09-23 12:50:31 +0530

    Created login and profile routes, created authMiddleware for profile, used cookieParser

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -20,3 +20,42 @@ export const createUserController = async (req, res) => {
         res.status(400).send(err.message)
     }
 }
+
+export const loginController = async (req, res) => {
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    try {
+        const {email, password} = req.body
+        const user = await userModel.findOne({email}).select('+password')
+
+        if(!user){
+            return res.status(401).json({
+                errors: 'Invaild credentials'
+            })
+        }
+
+        const isMatch = await user.isValidPassword(password)
+        if(!isMatch){
+            return res.status(401).json({
+                errors: 'Invalid credentials'
+            })
+        }
+
+        const token = await user.generateJWT()
+        res.status(200).json({ user, token})
+    } catch(err) {
+        res.status(400).send(err.message)
+    }
+}
+
+export const profileController = async (req, res) => {
+
+    console.log(req.user)
+    res.status(200).json({
+        user: req.user
+    })
+}
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,16 +1,22 @@
-import {Router} from 'express'
+import { Router } from 'express'
 import * as userController from '../controllers/user.controllers.js'
 import { body } from 'express-validator'
+import * as authMiddleware from '../middleware/auth.middleware.js'
 
 const router = Router()
 
 router.post(
     '/register',
     body('email').isEmail().withMessage('Email must be a vaild email address'),
-    body('password').isLength({min: 3}).withMessage('Password must be at least 3 characters'),
+    body('password').isLength({ min: 3 }).withMessage('Password must be at least 3 characters'),
 
     userController.createUserController)
 
+router.post('/login',
+    body('email').isEmail().withMessage('Email must be a vaild email address'),
+    body('password').isLength({ min: 3 }).withMessage('Password must be at least 3 characters'),
+    userController.loginController)
 
+router.get('/profile', authMiddleware.authUser, userController.profileController)
 
-export default router
+export default router 
