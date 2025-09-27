f6fca47f6d2c0a23c198e23eeceacf70f22477b5
Sourya <souryagupta2005@gmail.com>
2025-09-27 22:26:24 +0530

    Created Autentication for frontend.

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -16,6 +16,8 @@ export const createUserController = async (req, res) => {
         const user = await userService.createUser(req.body)
 
         const token = await user.generateJWT()
+
+        delete user._doc.password
         res.status(201).json({ user, token })
     } catch(err) {
         res.status(400).send(err.message)
@@ -47,6 +49,9 @@ export const loginController = async (req, res) => {
         }
 
         const token = await user.generateJWT()
+
+        delete user._doc.password
+
         res.status(200).json({ user, token})
     } catch(err) {
         res.status(400).send(err.message)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import AppRoutes from './routes/AppRoutes.jsx'
+import { UserProvider } from './context/user.context.jsx'
 
 const App = () => {
-  return <AppRoutes />
+  return (
+    <UserProvider>
+      <AppRoutes />
+    </UserProvider>
+  )
 }
 
 export default App
