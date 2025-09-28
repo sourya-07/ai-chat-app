2edc99d3349ae266f93b16e0c797a9bffdfd73da
Sourya <souryagupta2005@gmail.com>
2025-09-28 05:39:42 +0530

    Project route, project service project controllers completed

---
+++ backend/models/project.model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+
+const projectSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        lowercase: true,
+        required: true,
+        trim: true,
+        unique: [true, 'Project name name must be unique'],
+
+    },
+
+    users: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+})
+
+const Project = mongoose.model('Project', projectSchema);
+
+export default Project;
