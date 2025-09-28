23262c9b1f5cc5713814434065d4b5605c04acfc
Sourya <souryagupta2005@gmail.com>
2025-09-28 05:57:42 +0530

    Desinged the frontend for project

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL
-});
 
-export default axiosInstance;
+const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        "Authorization": `Bearer ${localStorage.getItem('token')}`
+    }
+})
+
+
+export default axiosInstance;   
