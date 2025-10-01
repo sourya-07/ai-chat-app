0bf94c70257e399daf1e79b8b757cc6afd762bf4
Sourya <souryagupta2005@gmail.com>
2025-10-02 04:10:13 +0530

    Created UserAuth and called into Home and project routes into UserAuth

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,15 +3,16 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import Login from '../screens/Login.jsx'
 import Register from '../screens/Register.jsx'
 import Home from '../screens/Home.jsx'
+import UserAuth from '../auth/UserAuth.jsx'
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<UserAuth><Home /></UserAuth>} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
-        {/* <Route path='/project' element={<Project />}/> */}
+        <Route path='/project' element={<UserAuth><Project /></UserAuth>} />
       </Routes>
     </BrowserRouter>
   )
