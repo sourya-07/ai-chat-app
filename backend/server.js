c5423b66fb9341fd4a775a70b09b29683f814411
Sourya <souryagupta2005@gmail.com>
2025-10-29 01:27:30 +0530

    Given the system instructions.

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { Server } from 'socket.io'
 import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose'
 import projectModel from './models/project.model.js'
+import {generateResult} from "./services/ai.service.js";
 
 const port = process.env.PORT || 3000
 
@@ -53,7 +54,27 @@ io.on('connection', socket => {
 
     console.log("A user connected");
     socket.join(socket.roomId)
-    socket.on('project-message', data => {
+    socket.on('project-message', async data => {
+
+        const message = data.message
+        const aiIsPresentInMessage = message.includes('@ai')
+        if (aiIsPresentInMessage) {
+            
+            const prompt = message.replace('@ai', '')
+            const result = await generateResult(prompt)
+
+            io.to(socket.roomId).emit('project-message', {
+                message: result,
+                sender: {
+                    _id: 'ai_bot',
+                    email: 'AI'
+                }
+            })
+
+
+            return
+        }
+
         socket.broadcast.to(socket.roomId).emit('project-message', data)
     })
     socket.on('disconnect', () => {
--- backend/services/ai.service.js
+++ backend/services/ai.service.js
@@ -3,7 +3,8 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_KEY)
 
 const model = genAI.getGenerativeModel({
-    model: "gemini-2.5-flash"
+    model: "gemini-2.5-flash",
+    systemInstruction: `You are an expert in MERN and Development, you always write code in modular and break the code in the possible way follow best practices, You use understandable comments in the code, You create files as needed, You write code while maintaining the working of previous code, You always write code that is scalable and maintainable, In your code you always handle the errors and exceptions.`
 })
 
 export const generateResult = async(prompt) => {
